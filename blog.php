cdac43eb39c64c1eb395342a8e2cbd9ce2383910
Satish463143 <166995533+Satish463143@users.noreply.github.com>
2024-04-27 10:00:17 +0545

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,153 @@
+<?php include"header.php" ?>
+
+
+<section class="main-body">
+    <section class="menupart">
+      <div class="container">
+          <div class="navbar">
+                <ul>
+                    <li><a class="vlink rounded border-0" href="index.php"><i class="fas fa-home"></i> <span>Home</span></a></li>
+                    <li><a class="vlink rounded" href="about.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" version="1.1">
+                            <path d="M 234.500 1.109 C 176.759 9.432, 127.291 53.670, 112.631 110.092 C 109.171 123.411, 107.999 133.088, 108.004 148.322 C 108.017 190.051, 124.261 227.348, 155.043 256.325 C 160.828 261.771, 163.725 265.145, 162.866 265.434 C 162.115 265.688, 156.325 267.259, 150 268.926 C 113.368 278.580, 79.804 294.406, 52.770 314.771 C 28.635 332.951, 14.304 351.969, 6.030 376.797 C 0.469 393.482, -0.166 399.787, 0.236 434.270 C 0.582 463.999, 0.711 465.806, 2.928 471.858 C 8.166 486.161, 19.991 499.458, 33.193 505.892 C 46.683 512.467, 30.657 512.050, 258.570 511.762 L 465.500 511.500 471.858 509.122 C 489.905 502.371, 504.576 486.935, 509.517 469.500 C 510.452 466.200, 511.618 462.719, 512.109 461.764 C 513.240 459.560, 513.262 398.039, 512.131 399.822 C 511.591 400.673, 511.065 399.207, 510.654 395.710 C 509.068 382.217, 500.710 360.827, 492.030 348.044 C 477.849 327.162, 454.797 308.673, 422.732 292.465 C 409.661 285.858, 407.328 285, 402.432 285 C 394.084 285, 387.658 288.888, 383.903 296.210 C 380.174 303.481, 382.103 314.117, 388.153 319.645 C 389.444 320.825, 395.196 324.133, 400.935 326.997 C 434.682 343.836, 454.473 360.827, 463.963 381.107 C 469.941 393.883, 471.178 401.162, 471.743 426.887 C 472.346 454.330, 471.657 459.160, 466.294 465.096 C 459.590 472.517, 475.334 472, 256 472 C 36.666 472, 52.410 472.517, 45.706 465.096 C 40.343 459.160, 39.654 454.330, 40.257 426.887 C 40.822 401.162, 42.059 393.883, 48.037 381.107 C 57.526 360.829, 79.017 342.601, 112.500 326.430 C 152.886 306.925, 192.193 298.331, 249.500 296.475 C 272.041 295.746, 281.041 294.712, 293.860 291.381 C 351.286 276.461, 395.180 226.603, 403.083 167.319 C 404.733 154.937, 403.693 128.011, 401.136 116.911 C 386.983 55.476, 337.635 9.566, 276.633 1.082 C 267.635 -0.170, 243.261 -0.154, 234.500 1.109 M 249 40.620 C 232.171 42.386, 220.517 45.628, 207.614 52.135 C 182.061 65.019, 162.581 87.738, 153.692 115.023 C 141.384 152.803, 151.534 195.297, 179.556 223.301 C 189.450 233.189, 197.087 238.620, 209.500 244.597 C 237.522 258.091, 268.170 259.099, 296.575 247.463 C 330.878 233.410, 355.794 202.401, 362.029 166 C 363.674 156.393, 363.674 139.607, 362.029 130 C 354.518 86.155, 319.707 50.544, 276.500 42.507 C 268.647 41.046, 254.311 40.062, 249 40.620 M 0.445 431 C 0.445 448.325, 0.582 455.413, 0.748 446.750 C 0.915 438.087, 0.915 423.913, 0.748 415.250 C 0.582 406.587, 0.445 413.675, 0.445 431" stroke="none" fill="#040404" fill-rule="evenodd"/>
+                         </svg>
+                    <span>About Me</span></a></li>
+                    <li><a class="vlink rounded" href="portfolio.php"><i class="fa-solid fa-address-card"></i> <span>Portfolio</span></a></li>
+                    <li  class="active"><a class="vlink rounded" href="blog.php"><i class="fa-solid fa-blog"></i>  <span>Blog</span></a></li>
+                    <li><a class="vlink rounded" href="contact.php"><i class="fa-solid fa-comments"></i><span>Contact</span></a></li>
+                    
+                </ul>
+          </div>
+      </div>
+    </section>
+</section>
+<section class="blog_banner">
+    <div class="blog_container"> 
+        <div class="banner_head">
+            <h2>Blog post</h2>
+        </div>
+        <div class="blog_grid">
+            <div class="blog_cont">
+                <div class="blog_cont_img">
+                    <img src="Image/time.jpg" alt="">
+                    <div class="blog_cont_deta">
+                        <div class="blog_read">
+                            <a href="https://www.amazon.com/Brief-History-Time-Black-Holes/dp/0553175211" target="_blank">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="details">
+                    <p>A Brief History of Time</p>
+                    <div class="details_cont">
+                        <p>Stephen William Hawking</p>
+                    </div>
+                    <div  class="details_img">
+                        <img src="Image/Stephen_William_Hawking.jpg" alt="">
+                    </div>                    
+                </div>
+             </div>
+             <div class="blog_cont">
+                <div class="blog_cont_img">
+                    <img src="Image/vanurability.png" alt="">
+                    <div class="blog_cont_deta">
+                        <div class="blog_read">
+                            <a href="https://www.ted.com/talks/brene_brown_the_power_of_vulnerability?language=en" target="_blank">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="details">
+                    <p>The Power of Vulnerability</p>
+                    <div class="details_cont">
+                        <p>Brené Brown</p>
+                    </div>
+                    <div  class="details_img">
+                        <img src="Image/Brene.jpg" alt="">
+                    </div>                    
+                </div>
+
+             </div>
+             <div class="blog_cont">
+                <div class="blog_cont_img">
+                    <img src="Image/design.jpg" alt="">
+                    <div class="blog_cont_deta">
+                        <div class="blog_read">
+                            <a href="https://en.wikipedia.org/wiki/The_Design_of_Everyday_Things" target="_blank">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="details">
+                    <p>The Design of Everyday Things</p>
+                    <div class="details_cont">
+                        <p>Don Norman</p>
+                    </div>
+                    <div  class="details_img">
+                        <img src="Image/don.webp" alt="">
+                    </div>                    
+                </div>
+
+             </div>
+             <div class="blog_cont">
+                <div class="blog_cont_img">
+                    <img src="Image/childhood_careers.webp" alt="">
+                    <div class="blog_cont_deta">
+                        <div class="blog_read">
+                            <a href="https://www.youtube.com/watch?v=ji5_MqicxSo" target="_blank">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="details">
+                    <p>The Last Lecture: Achieving Your Childhood Dreams</p>
+                    <div class="details_cont">
+                        <p>Randy Pausch</p>
+                    </div>
+                    <div  class="details_img">
+                        <img src="Image/images.jpeg" alt="">
+                    </div>                    
+                </div>
+
+             </div>
+             <div class="blog_cont">
+                <div class="blog_cont_img">
+                    <img src="Image/money-1.jpg" alt="">
+                    <div class="blog_cont_deta">
+                        <div class="blog_read">
+                            <a href="https://paulgraham.com/essay.html" target="_blank">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="details">
+                    <p>The Age of the Essay</p>
+                    <div class="details_cont">
+                        <p>Paul Graham</p>
+                    </div>
+                    <div  class="details_img">
+                        <img src="Image/bio-17.jpeg" alt="">
+                    </div>                    
+                </div>
+
+             </div>
+             <div class="blog_cont">
+                <div class="blog_cont_img">
+                    <img src="Image/ciobulletin.webp" alt="">
+                    <div class="blog_cont_deta">
+                        <div class="blog_read">
+                            <a href="https://www.ted.com/talks/simon_sinek_how_great_leaders_inspire_action?language=en" target="_blank">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="details">
+                    <p>How Great Leaders Inspire Action</p>
+                    <div class="details_cont">
+                        <p>Simon Sinek </p>
+                    </div>
+                    <div  class="details_img">
+                        <img src="Image/simon-sinek.jpg" alt="">
+                    </div>                    
+                </div>
+
+             </div>
+        </div>
+    </div>
+</section>
+
+<?php include"footer.php" ?>
